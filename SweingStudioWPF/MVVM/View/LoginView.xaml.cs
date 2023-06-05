6cc8c42bb75670a066344a6a647357aa68198d86
Alexey Taradoyna <AltarNatsev@yandex.ru>
2023-06-05 16:14:32 +0700

    Log in
    Added UI for authorization window

---
+++ SweingStudioWPF/MVVM/View/LoginView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SweingStudioWPF.MVVM.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton==MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Minimize_btn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState= WindowState.Minimized;
+        }
+
+        private void Close_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Login_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
