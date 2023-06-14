f20e023033e06052dc495f414a5a8ce2d685bc6a
Alexey Taradoyna <AltarNatsev@yandex.ru>
2023-06-14 12:01:03 +0700

    ViewModel
    Created LoginViewModel and ViewModelCommand. Written check for user authorization

---
+++ SweingStudioWPF/MVVM/ViewModel/LoginViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Input;
+
+namespace SweingStudioWPF.MVVM.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        //Fields
+        private string _username;
+        private SecureString _password;
+        private string _errorMessege;
+        private bool _isViewVisable = true;
+
+        //Properties
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessege
+        {
+            get
+            {
+                return _errorMessege;
+            }
+            set
+            {
+                _errorMessege = value;
+                OnPropertyChanged(nameof(ErrorMessege));
+            }
+        }
+        public bool IsViewVisable
+        {
+            get
+            {
+                return _isViewVisable;
+            }
+            set
+            {
+                _isViewVisable = value;
+                OnPropertyChanged(nameof(IsViewVisable));
+            }
+        }
+
+        // Commands
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        //Constructor
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p => ExecuteRecoverPassCommand("", ""));
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || Password.Length < 3 || Password == null)
+                validData = false;
+            else
+                validData = true;
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteRecoverPassCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
